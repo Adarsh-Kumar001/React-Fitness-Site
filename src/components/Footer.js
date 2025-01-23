74c981ac7ff7d37ea6aa04b5026048bec9ddf955
Adarsh <adarshkumar102004@gmail.com>
2025-01-23 14:39:51 +0530

    minor bug fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className='w-[full] bg-blue-500 flex justify-center gap-5 content-center p-5'>
       <img className='w-20' src={GymLogo} alt='Gym-logo'/>
-      <h1 className='my-auto font-bold'>Ftiness Site &copy; 2025</h1>
+      <h1 className='my-auto font-bold'>Fitness Site &copy; 2025</h1>
     </div>
   )
 }
